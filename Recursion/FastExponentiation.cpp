9cc9861ccc64a2f264c2b84c18e5a2ec07e0354d
Josh Burbridge <39570764+jburbridge@users.noreply.github.com>
2018-06-21 16:12:17 -0500

    Expanded upon fast exponentiation example

--- Recursion/FastExponentiation.cpp
+++ Recursion/FastExponentiation.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int fastExp(int x, int p)
+int fast_exp_calls = 0;
+int faster_exp_calls = 0;
+
+double fastExp(int x, int p)
 {
+    fast_exp_calls++;
+
     if(p == 0)
         return 1;
 
@@ -16,11 +21,38 @@ int fastExp(int x, int p)
         return x * fastExp(x, p/2)  * fastExp(x, p/2);
 }
 
+double fasterExp(int x, int p)
+{
+    faster_exp_calls++;
+
+    if(p == 0)
+        return 1;
+
+    else if(p == 1)
+        return x;
+
+    else if(p % 2 == 0)
+    {
+        double result = fasterExp(x, p/2);
+        return result * result;
+    }
+
+    else
+    {
+        double result = fasterExp(x, p/2);
+        return x * result * result;
+    }
+}
+
 int main()
 {
     int x = 5;
-    int p = 9;
+    int p = 100;
 
     cout << x << "^" << p << " = " << fastExp(x, p) << "\n";
+    cout << x << "^" << p << " = " << fasterExp(x, p) << "\n";
+
+    cout << "Exp calls: " << fast_exp_calls << "\n";
+    cout << "Fast Exp calls: " << faster_exp_calls << "\n";
     return 0;
 }
